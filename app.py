ef8094f56c137595333b171d6c7fc6032dce2043
Degan906 <degan906@gmail.com>
2025-06-22 15:26:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from modules.clientes import tela_clientes, tela_busca_edicao_clientes
 from modules.veiculos import tela_veiculos
 from modules.manutencoes import tela_manutencoes
+from modules.veiculos import tela_veiculos, tela_busca_edicao_veiculos
+
 
 st.set_page_config(page_title="SID - Sistema de Manutenção", layout="wide")
 st.title("🚗 SID - Sistema de Manutenção de Veículos")
@@ -20,5 +22,8 @@ elif menu == "Buscar/Editar Clientes":
     tela_busca_edicao_clientes()
 elif menu == "Cadastro de Veículos":
     tela_veiculos()
+elif menu == "Buscar/Editar Veículos":
+    tela_busca_edicao_veiculos()
 elif menu == "Cadastro de Manutenções":
     tela_manutencoes()
+
